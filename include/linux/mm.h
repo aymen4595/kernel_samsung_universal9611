448d5c4013aff269a3f7adb0802cb96083cb41cc
aymen4595 <aymen4595@gmail.com>
2026-04-27 21:03:47 +0200

    remit mm.h dorigine

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2657,7 +2657,7 @@ void __init setup_nr_node_ids(void);
 static inline void setup_nr_node_ids(void) {}
 #endif
 
-extern bool need_memory_boosting(struct pglist_data *pgdat);
+extern inline bool need_memory_boosting(struct pglist_data *pgdat);
 
 enum memsize_kernel_type {
 	MEMSIZE_KERNEL_KERNEL = 0,
